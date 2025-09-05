9cc15e602c20f86926a1bfad6a4187abeb1f1214
maveancog-dtchkd <mave.ancog@dorsu.edu.ph>
2025-09-05 18:47:57 +0800

    Create Exer1_Functional.java

---
+++ Exer1_Functional.java
@@ -0,0 +1,9 @@
+public class Functional {
+    public static int add(int a, int b) {  // Function to add two numbers
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("Sum = " + result);
+    }
+}
