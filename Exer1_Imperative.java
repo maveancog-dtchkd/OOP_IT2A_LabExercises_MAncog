07c4f8ca61a8dff0612724d904e15165241018cf
maveancog-dtchkd <mave.ancog@dorsu.edu.ph>
2025-09-05 19:38:16 +0800

    Delete Exer1_Imperative.java

--- Exer1_Imperative.java
+++
@@ -1,7 +0,0 @@
-public class Imperative {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 12; i++) {
-            System.out.println("Number: " + i);
-        }
-    }
-}
