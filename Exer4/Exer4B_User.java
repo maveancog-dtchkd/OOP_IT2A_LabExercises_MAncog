c7f73a0aedc6ba2c08522284e0ebf0e9e05c9872
maveancog-dtchkd <mave.ancog@dorsu.edu.ph>
2025-12-04 01:45:56 +0800

    Create Exer4B_User.java

---
+++ Exer4/Exer4B_User.java
@@ -0,0 +1,19 @@
+public class User {
+    protected String name;
+    protected String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Polymorphic Method (overridden in subclasses)
+    public void accessSystem() {
+        System.out.println(name + " is accessing the system.");
+    }
+
+    // Overloaded method (method overloading)
+    public void accessSystem(String time) {
+        System.out.println(name + " accessed the system at: " + time);
+    }
+}
