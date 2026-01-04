d91fd82f4247b1e811c74038c26162c73eba8c0a
maveancog-dtchkd <mave.ancog@dorsu.edu.ph>
2026-01-04 15:12:43 +0800

    Delete Exer3/Exer3B_DisapprovedAppointment.java

--- Exer3/Exer3B_DisapprovedAppointment.java
+++
@@ -1,7 +0,0 @@
-public class DisapprovedAppointment extends Appointment {
-
-    public DisapprovedAppointment(String patientName, String date) {
-        super(patientName, date);
-        this.status = "DISAPPROVED";
-    }
-}
